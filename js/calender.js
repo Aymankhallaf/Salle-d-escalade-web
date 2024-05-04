17fff1be6b611c33d76f5e2fb918a243ca7fe9b6
khallaf <engayman.khalaf@gmail.com>
2024-05-04 13:38:34 +0200

    fix sinday bug

--- js/calender.js
+++ js/calender.js
@@ -3,79 +3,64 @@ let display = document.querySelector(".calender__ttl");
 let days = document.querySelector(".calender__month");
 let previous = document.querySelector(".calender__left");
 let next = document.querySelector(".calender__right");
+
+//selected date
 let selected = document.querySelector(".calender__selected-txt");
+let closedDays = "";
 
-let today = new Date();
+let
+  day = new Date();
 
-let year = today.getFullYear();
-let month = today.getMonth();
+let year =
+  day.getFullYear();
+let month =
+  day.getMonth();
 
 
 /**
  * displays the calender header (month and year).
- * @returns {Array} return array the first element day index
+ * @returns {Array} return array the first element day index 
  */
 function displayCalendarHeader() {
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
   // Sunday - Saturday : 0 - 6
-  const firstDayIndex = firstDay.getDay();
-  const numberOfDays = lastDay.getDate();
-  let formattedDate = today.toLocaleString("fr-FR", {
-    year: "numeric",
-    month: "long",
-  });
+  let firstDayIndex = firstDay.getDay();
+  let numberOfDays = lastDay.getDate();
+  let formattedDate =
+    day.toLocaleString("fr-FR", {
+      year: "numeric",
+      month: "long",
+    });
 
   display.innerHTML = `${formattedDate}`;
   return [firstDayIndex, numberOfDays]
 }
 
-
-function createEmbtyDays(element) {
+function createDaycell(element, dateset) {
   const days = document.getElementById("day-template");
   const day = document.importNode(days.content, true);
   const monthDays = document.getElementById("month-day")
   day.querySelector(".js-calender__month--day").textContent = element;
+  day.querySelector(".js-calender__month--day").dataset.date = dateset;
   monthDays.appendChild(day);
 
 }
 
-
-let firstDayIndex = displayCalendarHeader()[0];
-let numberOfDays = displayCalendarHeader()[1];
-
-function showEmptyDay() {
-  for (let x = 1; x <= firstDayIndex; x++) {
-    createEmbtyDays("");
+function showEmptyDay(firstDayIndex) {
+  if(firstDayIndex === 0) {firstDayIndex = 7 } ;
+  for (let x = 0; x < firstDayIndex - 1; x++) {
+    createDaycell("")
   }
 }
 
-function createEmbtydays(element,dateset) {
-  const days = document.getElementById("day-template");
-  const day = document.importNode(days.content, true);
-  const monthDays = document.getElementById("month-day")
-  day.querySelector(".js-calender__month--day").textContent = element;
-  day.querySelector(".js-calender__month--day").dateset = dateset;
-  day.querySelector(".js-calender__month--day").datetime = dateset;
-  monthDays.appendChild(day);
-
-}
-
-
-//show empty days field
-showEmptyDay();
-
-
 //create days in the month
-
-
-function displayCalendar() {
+function displayCalendar(numberOfDays) {
 
   for (let i = 1; i <= numberOfDays; i++) {
-    let li = document.createElement("li");
     let currentDate = new Date(year, month, i);
 
-    li.dataset.date = currentDate.toLocaleString("fr-FR", {
+    let dataSet = currentDate.toLocaleString("fr-FR", {
       weekday: "long",
       year: "numeric",
       month: "long",
@@ -83,29 +68,32 @@ function displayCalendar() {
 
     });
 
-
-    li.classList = "calender__month--day";
-
-    li.innerHTML += i;
-    days.appendChild(li);
-    if (
-      currentDate.getFullYear() === new Date().getFullYear() &&
-      currentDate.getMonth() === new Date().getMonth() &&
-      currentDate.getDate() === new Date().getDate()) {
-      li.classList.add("current-date");
-    }
+    createDaycell(i, dataSet);
   }
-}
 
-// Call the function to display the calendar
+}
 displayCalendar();
 
+function upDateDate() {
+  displayCalendarHeader();
+  let firstDayIndex = displayCalendarHeader()[0];
+  console.log(firstDayIndex);
+  let numberOfDays = displayCalendarHeader()[1];
+  showEmptyDay(firstDayIndex)
+  displayCalendar(numberOfDays)
+
+
+}
+
+
+
+
+upDateDate()
 
 
 previous.addEventListener("click", () => {
-  days.innerHTML = "";
-  selected.innerHTML = "";
 
+  days.innerHTML = ""
   if (month < 0) {
     month = 11;
     year = year - 1;
@@ -113,31 +101,33 @@ previous.addEventListener("click", () => {
 
   month = month - 1;
 
-  date.setMonth(month);
-
-  displayCalendar();
+  day.setMonth(month);
+  upDateDate()
   displaySelected();
   disactiveClosedDay("lu");
+
 });
 
 next.addEventListener("click", () => {
-  days.innerHTML = "";
-  selected.innerHTML = "";
 
+  days.innerHTML = ""
   if (month > 11) {
     month = 0;
     year = year + 1;
   }
 
   month = month + 1;
-  date.setMonth(month);
 
-  displayCalendar();
-  displaySelected();
+  day.setMonth(month);
+
+  upDateDate()
   disactiveClosedDay("lu");
 
-
 });
+
+
+
+
 
 const dayElements = document.querySelectorAll(".calender__month--day");
 function handleClick(e) {
@@ -149,6 +139,10 @@ function handleClick(e) {
   selected.innerHTML = `Vous avez choisi: ${selectedDate.dataset.date}`;
 
 }
+
+
+
+
 
 function displaySelected() {
   dayElements.forEach((day) => {
