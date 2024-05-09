316e11d6ef90970014a35dd44c50c3568f2e64d7
khallaf <engayman.khalaf@gmail.com>
2024-05-09 18:54:53 +0200

    add main menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-import "./reservation/numberOfParticpant.js"
-import "./reservation/calender.js"
-import "./reservation/hours.js"
-import "./reservation/reservation.js"
-import "./form.js"
 
 
+//menu toggle
 
+document.getElementById("header-nav__btn").addEventListener("click", () => {
+    console.log("clicked")
+    document.getElementById("main-menu").classList.toggle("header-nav__menu--side");
+    document.querySelectorAll("header-nav__menu-link--side")
+});
