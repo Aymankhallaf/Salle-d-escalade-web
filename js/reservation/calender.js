c7a1e791958d52f4259fee08bccdc3d57a559fba
khallaf <engayman.khalaf@gmail.com>
2024-05-12 20:26:36 +0200

    add comments to calendar.js

--- js/reservation/calender.js
+++ js/reservation/calender.js
@@ -2,24 +2,32 @@
 const closedDay = ["lu"]
 const holydays = ["jeudi 9 mai 2024", "Lundi 20 mai 2024", "dimanche 14 juillet 2024", "jeudi 15 août 2024"]
 
-//calender header
-let display = document.getElementById("calender__ttl");
-let daysContainer = document.getElementById("month-days");
 let previous = document.getElementById("calender__left");
 let next = document.getElementById("calender__right");
 
-//selected date
-let chosenDate = null;
+// all the days apears here
+let daysContainer = document.getElementById("month-days");
+
+
+
+
+// selected date apears here
 let selected = document.querySelector(".calender__selected-txt");
+// constant of today
 const today = new Date();
+// avariable of day changes every time we change the month to calculate the calender the initial start is today.
 let currentDate = new Date();
 let year = currentDate.getFullYear();
 let month = currentDate.getMonth();
 
+//selected date
+let chosenDate = null;
+
+
 
 /**
- * 
- * @returns{array} array, the first day index [sunday-satrday 0-6] and the number of days in the month
+ * get the index of the first day the current month and the number of days in this month.
+ * @returns{array} array, the first day index [ex. sunday-satrday 0-6] and the number of days in the month
  */
 function getFirstAndLastDay() {
   const firstDay = new Date(year, month, 1);
@@ -31,21 +39,22 @@ function getFirstAndLastDay() {
 
 
 
-
-
 /**
- * displays the calender header (month and year).
- * @returns {Array} return array the first element day index 
+ * Displays the calender header (month and year) ex. mai 2024.
  */
 function displayCalendarHeader() {
-  display.innerHTML = currentDate.toLocaleString("fr-FR", {
+  document.getElementById("calender__ttl").innerHTML = currentDate.toLocaleString("fr-FR", {
     year: "numeric",
     month: "long",
   });
-  // to be used for database
-  display.dataset.month = display.innerHTML;
 }
 
+
+/**
+ * creates a day cell with its attributes and disactive it according to isDayDisactive function.
+ * @param {number} day a number of day.
+ * @param {*} currentDate a variable of day change   
+ */
 function createDayCell(day, currentDate) {
   let dataSet = currentDate.toLocaleString("fr-FR", {
     weekday: "long",
@@ -64,36 +73,53 @@ function createDayCell(day, currentDate) {
     dayCell.classList.add("disactive");
   }
 
-
   daysContainer.appendChild(dayElement);
 }
 
+/**
+ * disactive the day according to holidays, closed days and previous days.     
+ * @param {text} dataSet text of the date
+ * @param {date} currentDate the curent date
+ * @returns 
+ */
 function isDayDisactive(dataSet, currentDate) {
-  return (closedDay.includes(dataSet.slice(0, 2))
-    || holydays.includes(dataSet) || (currentDate < today))
+  return (
+    closedDay.includes(dataSet.slice(0, 2))
+    || holydays.includes(dataSet)
+    || (currentDate < today))
 
 }
 
-function showEmptyDay(firstDayIndex) {
 
+
+/**
+ * create empty cells in the clender where there is no date in the begining of the month.
+ * @param {number} firstDayIndex 
+ */
+function showEmptyDay(firstDayIndex) {
   if (firstDayIndex === 0) { firstDayIndex = 7 };
   for (let x = 0; x < firstDayIndex - 1; x++) {
     createDayCell("", "")
   }
 }
 
-//create days in the month
+/**
+ * create cells and display them and listen to the events in this cells check createDayCell() and updateEventListeners() functions.
+ * @param {number} numberOfDays number of days in the month.
+ */
 function displayCalendar(numberOfDays) {
   for (let i = 1; i <= numberOfDays; i++) {
     let currentDate = new Date(year, month, i);
-
     createDayCell(i, currentDate);
   }
   updateEventListeners();
 }
 
 
-function upDateDate() {
+/**
+ * updates calender diplay with all functions. See more showEmptyDay() and displaydisplayCalendar()
+ */
+function updateCalendar() {
   displayCalendarHeader();
   let firstDayIndex = getFirstAndLastDay()[0];
   let numberOfDays = getFirstAndLastDay()[1];
@@ -101,10 +127,10 @@ function upDateDate() {
   displayCalendar(numberOfDays)
 }
 
+updateCalendar()
 
-upDateDate()
 
-
+//listens to pervious button to display calender
 previous.addEventListener("click", () => {
   daysContainer.innerHTML = ""
   if (month < 0) {
@@ -113,11 +139,11 @@ previous.addEventListener("click", () => {
   }
   month--;
   currentDate.setFullYear(year, month);
-  upDateDate();
+  updateCalendar();
 });
 
 
-
+//listens to next button to display calender
 next.addEventListener("click", () => {
   daysContainer.innerHTML = ""
   if (month > 11) {
@@ -126,24 +152,31 @@ next.addEventListener("click", () => {
   }
   month++;
   currentDate.setFullYear(year, month);
-  upDateDate();
+  updateCalendar();
 
 });
 
 
-
+/**
+ * listen to the click of active days celles and applicate handleCalendarCellClick() function.
+ */
 function updateEventListeners() {
   const dayElements = daysContainer.querySelectorAll(".js-calender__month--day");
   dayElements.forEach(dayElement => {
     if (dayElement.classList.contains("disactive")) {
-      dayElement.removeEventListener("click", handleClick);
+      dayElement.removeEventListener("click", handleCalendarCellClick);
     } else {
-      dayElement.addEventListener("click", handleClick);
+      dayElement.addEventListener("click", handleCalendarCellClick);
     }
   });
 }
 
-function handleClick(e) {
+
+/**
+* handles a Calendar Cell click and shown the selected day and disactive the other clicke day. 
+* @param {Event} a clicked event.
+*/
+function handleCalendarCellClick(e) {
   const selectedDate = e.target;
   activeDay(selectedDate);
   if (selectedDate.classList.contains("active")) {
@@ -153,6 +186,7 @@ function handleClick(e) {
     selected.innerHTML = `Vous avez choisi: ${chosenDate}`;
     selected.classList.remove("error");
     selected.dataset.selectedDay = chosenDate;
+    //review
     return chosenDate
 
   } else {
--- js/reservation/hours.js
+++ js/reservation/hours.js
@@ -4,6 +4,11 @@ const today = new Date();
 let timeNow = today.getHours();
 
 
+/**
+ * create an hour cell in html tag with attributes content, datasethour, datatset.minute and data time and disactive the cell acording to disactive function;
+ * @param {number} hour an hour in number formate
+ * @param {number} minute minutes in number formate 
+ */
 function createHourCell(hour, minute) {
     const template = document.getElementById("hours-template");
     const hourElement = document.importNode(template.content, true);
@@ -19,7 +24,11 @@ function createHourCell(hour, minute) {
 
 }
 
-
+/**
+ * display hours in html tag from start hour to end hour avery 30 min.
+ * @param {number} start the Starting hour in number format.
+ * @param {number} end the Starting hour in number format.
+ */
 function displayHour(start, end) {
     for (start; start < end; start++) {
         for (let minute = 0; minute < 60; minute += 30) {
@@ -27,10 +36,14 @@ function displayHour(start, end) {
         }
     }
     createHourCell(end, '00')
-
 }
 
 
+/**
+ * check if an hour is earier than the time now and return false. 
+ * @param {number} hour a certain hour in format number.
+ * @returns {boolean} return "true" if time now i or true 
+ */
 function isHoursDisactive(hour) {
     return timeNow > hour
 
