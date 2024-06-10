2a726397b9916a51e168f64c56771ef89a2f2e9d
khallaf <engayman.khalaf@gmail.com>
2024-06-10 21:25:25 +0200

    modification fontsize desktop

--- js/form.js
+++ js/form.js
@@ -1,4 +1,18 @@
 
+/**
+ * toggel function 
+ * @param {string} toggleId an id text ex(#features)
+ * @param {string} hiddenMenuClass a class text ex(.menu)
+ */
+function toggle(toggleId, hiddenMenuClass) {
+    document.querySelector(toggleId).addEventListener("click", function (event) {
+        btn.classList.toggle("closed");
+        document.querySelector(hiddenMenuClass).classList.toggle("text-hidden");
+    });
+}
+
+
+
 function toggle(element, closedClassName) {
     element.addEventListener("click", function (event) {
         element.classList.toggle(closedClassName);
@@ -13,6 +27,5 @@ function toggleClassName(className, closedClassName) {
 
 }
 
-toggleClassName(".js-select", "open-simple-arrow");
-
+toggleClassName(".js-select", "simple-arrow");
 
