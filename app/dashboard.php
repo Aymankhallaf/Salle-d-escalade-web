d5dfbfa7de2948307584c33e8cd522e0f3f191a8
khallaf <engayman.khalaf@gmail.com>
2024-07-25 00:09:30 +0200

    dashboard reservation details html & css

--- app/dashboard.php
+++ app/dashboard.php
@@ -1,39 +1,36 @@
 <?php
 include 'includes/_header.php';
 ?>
-<main>
+<main class="dashboard-reservation"  >
 
     <h1 class="dashboard-ttl" id="reservation">Réservation</h1>
     <nav>
         <ul class="dashboard-menu">
             <li><a class="dashboard-menu__a" href="#">Profil</a></li>
-            <li><a class="dashboard-menu__a dashboard-menu__a-current" href="#">Réservation</a></li>
+            <li><a class="dashboard-menu__a dashboard-menu__a--current" href="#">Réservation</a></li>
             <li><a class="dashboard-menu__a" href="#">Abonnement</a></li>
         </ul>
     </nav>
-    <section>
-        <h2 class="dashboard-ttl" id="reservation">Réservation détails:</h2>
-
-
-
-        <table>
-            <tr>
+    <section class="reservation-details">
+        <h2 class="reservation-details-tll" id="reservation-details-tll"><a href="#reservation-details">Réservation détails:</a></h2>
+        <table class="reservation-details-table">
+            <tr class="reservation-details-raw">
                 <th>Salle</th>
                 <td>Salle1</td>
             </tr>
-            <tr>
+            <tr class="reservation-details-raw">
                 <th>Date de réservation</th>
                 <td>14/07/2024 8:30</td>
             </tr>
-            <tr>
+            <tr class="reservation-details-raw">
                 <th>Durée</th>
                 <td>1h</td>
             </tr>
-            <tr>
+            <tr class="reservation-details-raw">
                 <th>Total Prix</th>
                 <td>50€</td>
             </tr>
-            <tr>
+            <tr class="reservation-details-raw">
                 <th>Statut</th>
                 <td>Payé</td>
             </tr>
