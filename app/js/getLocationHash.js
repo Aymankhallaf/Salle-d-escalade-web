90d719793bfda8add42a455656968f52c041b4ea
khallaf <engayman.khalaf@gmail.com>
2024-07-28 23:59:18 +0200

    send post request to get details of a reservation ok

--- app/js/getLocationHash.js
+++ app/js/getLocationHash.js
@@ -8,4 +8,15 @@ console.log("ok");
 document.addEventListener("DOMContentLoaded", function () {
     let param = F.getQueryParams()
     console.log(param);
+    F.callApi("POST", {
+        action: "getAReservation",
+        token: F.getToken(),
+        idReservation: param.idReservation
+
+    }).then(data => {
+        if (!data.isOk) {
+            displayError(data['errorMessage']);
+
+        }
+    });
 });
