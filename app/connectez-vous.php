dd28a30a8d4fb9076ea90a9e971af7e3b754bb29
khallaf <engayman.khalaf@gmail.com>
2024-08-08 12:02:10 +0200

    connectez-vous.php html css ok

--- app/connectez-vous.php
+++ app/connectez-vous.php
@@ -10,10 +10,12 @@ echo getHtmlErrors($errors);
 <ul id="messagesList" class="messages"></ul>
 
 <main class="connection">
+    <section>
+
         <h1 class="connection-ttl" id="connection">Connectez-vous à votre compte</h1>
         <form id="connection-form" class="connection-form" aria-label="formulaire de connexion" method="post">
             <h2 class="connection-form__ttl">Vous êtes déjà client</h2>
-            <ul>
+            <ul class="connection-form-ul">
                 <li class="connection-form__email">
                     <label class="connection-form__email-label" for="email">Votre email</label>
                     <input class="connection-form__email-input" type="email" name="email" id="connection-email" required />
@@ -23,14 +25,18 @@ echo getHtmlErrors($errors);
                     <input class="connection-form__password-input" type="password" name="password" id="connection-password" required />
                 </li>
             </ul>
-            <a href="connection-form__lnk">cliquez ici si vous avez oublié votre mot de passe</a>
-            <button id="connexion" type="submit" class="connection-form__password">connexion</button>
+            <a href="" class="connection-form__lnk">cliquez ici si vous avez oublié votre mot de passe</a>
+            <button id="connexion" type="submit" class="connection-form__btn">connexion</button>
 
         </form>
-        <h2 class="connection-form__ttl">Vous n'êtes pas encore client</h2>
-        <a href=""> Inscrivez vous</a>
+    </section>
+    <section>
 
-    </main>
+        <h2 class="signup__ttl">Vous n'êtes pas encore client</h2>
+        <a  class="signup__lnk" href="/inscrivez-vous.php"> Inscrivez vous</a>
+    </section>
+
+</main>
 
 
 <?php
