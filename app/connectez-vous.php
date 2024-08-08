3dde32a0f44c6a118c7c9775c2c7fd78ec11d473
khallaf <engayman.khalaf@gmail.com>
2024-08-08 09:47:07 +0200

    connectez-vous.php css form ok

--- app/connectez-vous.php
+++ app/connectez-vous.php
@@ -9,9 +9,9 @@ echo getHtmlErrors($errors);
 <ul id="errorsList" class="error"></ul>
 <ul id="messagesList" class="messages"></ul>
 
-<main>
+<main class="connection">
         <h1 class="connection-ttl" id="connection">Connectez-vous à votre compte</h1>
-        <form id="connection-form" class="connection-form" aria-label="formulaire de connexion" method="get">
+        <form id="connection-form" class="connection-form" aria-label="formulaire de connexion" method="post">
             <h2 class="connection-form__ttl">Vous êtes déjà client</h2>
             <ul>
                 <li class="connection-form__email">
@@ -23,7 +23,7 @@ echo getHtmlErrors($errors);
                     <input class="connection-form__password-input" type="password" name="password" id="connection-password" required />
                 </li>
             </ul>
-            <a href="">cliquez ici si vous avez oublié votre mot de passe</a>
+            <a href="connection-form__lnk">cliquez ici si vous avez oublié votre mot de passe</a>
             <button id="connexion" type="submit" class="connection-form__password">connexion</button>
 
         </form>
