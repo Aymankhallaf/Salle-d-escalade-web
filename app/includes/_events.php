700298994fc4b66bcffc3a64a33923e2b90ef04b
khallaf <engayman.khalaf@gmail.com>
2024-08-10 12:17:08 +0200

    add href to categories in events.php

--- app/includes/_events.php
+++ app/includes/_events.php
@@ -12,14 +12,14 @@ $categories =  getCategories($dbCo);
             <h3 class="card__ttl card__ttl--h3"><?= $categories["1"]["name"] ?></h3>
             <p class="card__txt"><?= $categories["1"]["description"] ?>
             </p>
-            <a target="_blank" href="category.php?id=<?= $category["1"]['id']; ?>" class="btn card__btn">En savoir plus</a>
+            <a target="_blank" href="category.php?id=<?= $category["1"]['id'];?>" class="btn card__btn">En savoir plus</a>
         </li>
         <li class="card card-events">
             <img class="card__img" src="img/competatiopn-s.webp" alt="Un homme participe à une compétition.">
             <h3 class="card__ttl card__ttl--h3"><?= $categories["2"]["name"] ?></h3>
             <p class="card__txt"><?= $categories["2"]["description"] ?>
             </p>
-            <a target="_blank" href="#" class="btn card__btn">En savoir plus</a>
+            <a target="_blank" href="category.php?id=<?= $category["2"]['id'];?>" class="btn card__btn">En savoir plus</a>
         </li>
 
         <li class="card card-events">
@@ -27,7 +27,7 @@ $categories =  getCategories($dbCo);
             <h3 class="card__ttl card__ttl--h3"><?= $categories["3"]["name"] ?></h3>
             <p class="card__txt"><?= $categories["3"]["description"] ?>
             </p>
-            <a target="_blank" href="#" class="btn card__btn">En savoir plus</a>
+            <a target="_blank" href="category.php?id=<?= $category["1"]['id'];?>" class="btn card__btn">En savoir plus</a>
         </li>
     </ul>
 </section>
