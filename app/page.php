ad4dd4fc4aead765e3ceab90505fd96414210df3
khallaf <engayman.khalaf@gmail.com>
2024-08-11 14:41:53 +0200

    pages css & html ok

--- app/page.php
+++ app/page.php
@@ -1,7 +1,20 @@
 <?php
 require_once 'includes/_startSession.php';
 include 'includes/_header.php';
+// var_dump($_GET);
+// $article = isset($_GET['article']);
+// var_dump(getArticleById($dbCo,$_GET['id']));
+?>
 
-var_dump($_GET);
-$article = isset($_GET['article']);
-var_dump(getArticleById($dbCo,$_GET['id']));
+<body>
+    <main class="page">
+        <source media="(min-width: 960px)" srcset="/img/robe-l.svg">
+        <img class="page-img" src="/img/robe.webp" alt="robe">
+        <h1 class="page-header">Masterclass : apprendre l’escalade avec des superstars de la discipline !</h1>
+        <p class="page-date">Publish date: <time datetime="25 octobre 2021">25 octobre 2021</time>.</p>
+        <p class="page-paragraph">Le fameux site américain Masterclass n’y pas avec le dos de la cuillère :
+            s’agissant de proposer une offre de formation en escalade, il a choisi deux superstars comme professeurs.
+            James Cameron qui vous enseigne la direction de film, Gordon Ramsay la cuisine, Gary Kasparov les échecs ou Natalie Portman le jeu d’acteur. Quoi de plus normal dans ce cas que l’escalade se découvre avec… Alex Honnold et Tommy Caldwell ! Connus pour leurs ascensions en duo mais aussi en solo sur les plus belles parois de la planète, les deux affirment détenir « 60 ans d’expériences combinées en escalade de haut niveau ».</p>
+
+    </main>
+</body>
