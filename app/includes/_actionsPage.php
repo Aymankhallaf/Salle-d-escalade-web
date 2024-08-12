f3638dfd17be6f133f1e7b1780e69e7280ecc2bf
khallaf <engayman.khalaf@gmail.com>
2024-08-12 19:11:36 +0200

    hide delete and edit for visters ok

--- app/includes/_actionsPage.php
+++ app/includes/_actionsPage.php
@@ -1,10 +1,15 @@
-
-
 <div class="articl-actions">
+    <?php if (isAdmin()) : ?>
+        <!-- Admins  can delete and edit -->
         <button id="articl-actions-delete" type="submit" class="articl-actions-delete-btn">Supprimer</button>
+    <?php elseif (isEditor()) : ?>
+        <!-- Editors can only edit -->
         <button id="articl-actions-edit" type="submit" class="articl-actions-edit-btn">Editer</button>
-    </div>
+    <?php endif; ?>
+</div>
 
+<?php if (isAdmin()) : ?>
+    <!-- Admins can see the delete form -->
     <form id="articl-delete-form" class="articl-delete-form hidden" method="POST" action="actions.php">
         <input type="hidden" id="token" name="token" value="<?= $_SESSION['token']; ?>">
         <label for="idPost">Vous voulez supprimer cette article !!</label>
@@ -14,11 +19,15 @@
             <button id="articl-delete-form-btn__cancel" type="button" class="articl-delete-form-btn__cancel">Annuler</button>
             <button id="articl-delete-form-btn__delete" type="submit" class="articl-delete-form-btn__delete">Supprimer</button>
         </div>
-
     </form>
+<?php endif; ?>
+
+<?php if (isEditor()) : ?>
+    <!-- Admins and Editors can see the edit form -->
     <form id="edit-form" class="signup-form" method="POST" action="actions.php">
         <input type="hidden" id="token" name="token" value="<?= $_SESSION['token'] ?>">
-        <input type="hidden" id="token" name="token" value="<?= $_SESSION['token'] ?>">
+        <input type="hidden" name="idPost" value="<?= $article['id_post']; ?>">
         <input type="hidden" name="action" value="editArticle">
         <button id="edit" type="submit" class="stepper-btn__next">edit</button>
     </form>
+<?php endif; ?>
--- app/page.php
+++ app/page.php
@@ -16,29 +16,13 @@ include 'includes/_header.php';
     <h1 class="page-header"><?= $article['title']; ?></h1>
     <p class="page-date">Publish date: <time datetime="<?= $article['date_post']; ?>"><?= $article['date_post'] ?></time>.</p>
     <p class="page-paragraph"><?= $article['paragraph']; ?></p>
+    <?php
+    if (isUserLoggedin() && isEditor()) {
+        include 'includes/_actionsPage.php';
+    }
 
-    <div class="articl-actions">
-        <button id="articl-actions-delete" type="submit" class="articl-actions-delete-btn">Supprimer</button>
-        <button id="articl-actions-edit" type="submit" class="articl-actions-edit-btn">Editer</button>
-    </div>
+    ?>
 
-    <form id="articl-delete-form" class="articl-delete-form hidden" method="POST" action="actions.php">
-        <input type="hidden" id="token" name="token" value="<?= $_SESSION['token']; ?>">
-        <label for="idPost">Vous voulez supprimer cette article !!</label>
-        <input type="hidden" name="idPost" value="<?= $article['id_post']; ?>">
-        <input type="hidden" name="action" value="deleteArticle">
-        <div class="articl-delete-form-btn">
-            <button id="articl-delete-form-btn__cancel" type="button" class="articl-delete-form-btn__cancel">Annuler</button>
-            <button id="articl-delete-form-btn__delete" type="submit" class="articl-delete-form-btn__delete">Supprimer</button>
-        </div>
-
-    </form>
-    <!-- <form id="edit-form" class="signup-form" method="POST" action="actions.php">
-        <input type="hidden" id="token" name="token" value="<?= $_SESSION['token'] ?>">
-        <input type="hidden" id="token" name="token" value="<?= $_SESSION['token'] ?>">
-        <input type="hidden" name="action" value="editArticle">
-        <button id="edit" type="submit" class="stepper-btn__next">edit</button>
-    </form> -->
 </main>
 
 
