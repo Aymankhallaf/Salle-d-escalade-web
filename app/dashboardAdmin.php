889c670295f7cb20964f4a7d43ace7e5a66be057
khallaf <engayman.khalaf@gmail.com>
2024-08-15 01:34:47 +0200

    start to create css  dashboardAdminphp

--- app/dashboardAdmin.php
+++ app/dashboardAdmin.php
@@ -1,31 +1,41 @@
-<main>
+<?php
+require_once 'includes/_startSession.php';
+require_once 'includes/_header.php';
+?>
+<main class="dashboard-admin">
     <h1>Tableau de bord d'administration</h1>
     <nav> </nav>
-    <section>
-        <h2>Tableau de bord d'administration</h2>
-        <h3>Remarques</h3>
-        <form action="">
-            <label for="start">Start date:</label>
-            <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
-            <label for="start">Start date:</label>
-            <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+    <h2 class="overview-ttl">Aperçu</h2>
+    <section class="static">
+        <h3 class="static__ttl">Remarques</h3>
+        <form class="static-form" action="">
+            <ul class="static-form-ul">
+                <li class="static-form__start">
+                    <label class="static-form__start-label" for="start-period">Date de début:</label>
+                    <input class="static-form__start-input" type="date" id="start-period" name="start-period" value="2018-07-22" />
+                </li>
+                <li class="static-form__end">
+                    <label class="static-form__end-label" for="end-period">Date de fin:</label>
+                    <input class="static-form__end-input" type="date" id="end-period" name="end-period" value="2018-07-22" min="2018-01-01" />
+                </li>
+            </ul>
         </form>
-        <ul>
-            <li>
-                <h4>Payé</h4>
-                <p>€ 100</p>
+        <ul class="static-result">
+            <li class="static-result-paid">
+                <h4 class="static-result-paid__ttl">Payé</h4>
+                <p class="static-result-paid__amount">€ 100</p>
             </li>
-            <li>
-                <h4>Participants actuels</h4>
-                <p>15</p>
+            <li class="static-result-Participants">
+                <h4 class="static-result-Participants__ttl">Participants actuels</h4>
+                <p  class="static-result-Participants__number">15</p>
             </li>
-            <li>
-                <h4>Montant impayé </h4>
-                <p>€ 100</p>
+            <li class="static-result-unpaid">
+                <h4 class="static-result-unpaid__ttl">Montant impayé </h4>
+                <p class="static-result-unpaid__amount">€ 100</p>
             </li>
-            <li>
-                <h4>Participants non payé</h4>
-                <p>€ 100</p>
+            <li class="static-result-comming-Participants">
+                <h4 class="static-result-comming-Participants__ttl">Participants non payé</h4>
+                <p class="static-result-comming-Participants__number">€ 100</p>
             </li>
         </ul>
 
@@ -33,3 +43,9 @@
     </section>
 
 </main>
+<?php
+include 'includes/_footer.php';
+?>
+</body>
+
+</html>
