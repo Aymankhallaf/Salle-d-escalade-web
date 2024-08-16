86dced2544d6982c9f5399048e4c6f3059e247be
khallaf <engayman.khalaf@gmail.com>
2024-08-16 22:17:39 +0200

    show the reservation in painer

--- app/panier.php
+++ app/panier.php
@@ -1,9 +1,6 @@
 <?php
 require_once 'includes/_startSession.php';
 
-// var_dump($_SESSION);
-
-// var_dump(getUserReservationHistory($dbCo,$_SESSION["idUser"]));
 
 //csfr protection
 if (!isServerOk()) {
@@ -16,13 +13,15 @@ if (!isUserLoggedin()) {
     addError("need_login");
     redirectToHeader("connectez-vous.php");
 }
-require_once 'includes/_config.php';
-
 include 'includes/_header.php';
+
+
 ?>
 <main class="dashboard">
+    <?php
     include 'includes/_notification.php';
-<h1 class="dashboard-ttl" id="reservation">Tableau de bord</h1>
+    ?>
+    <h1 class="dashboard-ttl" id="reservation">Panier</h1>
 
     <!-- reservation -->
     <section id="reservation" class="tab-dashboard reservation-details">
@@ -31,51 +30,81 @@ include 'includes/_header.php';
         </div>
     </section>
 
-<script type="module" src="./js/reservationDetails.js"></script>
+    <script type="module" src="./js/reservationDetails.js"></script>
+
+    <?php
+    include 'includes/_footer.php';
+    ?>
+
+    <!-- template reservation -->
+
+    <template id="template-reservation">
+        <table id="reservation-details-table" class="reservation-details-table">
+            <tr class="reservation-details-raw">
+                <th>Salle</th>
+                <td id="gym">Salle1</td>
+            </tr>
+            <tr class="reservation-details-raw">
+                <th>Date de réservation</th>
+                <td id="dateReservation">14/07/2024 8:30</td>
+            </tr>
+            <tr class="reservation-details-raw">
+                <th>Durée</th>
+                <td id="duration">1h</td>
+            </tr>
+            <tr class="reservation-details-raw">
+                <td id="totalPrix">50€</td>
+            </tr>
+            <tr class="reservation-details-raw">
+                <th>Statut</th>
+                <td id="status">modifier</td>
+            </tr>
+        </table>
+        <ul class="reservation-details-raw">
+            <li id="reservation-edit"><a class="reservation-edit">modifier</a></li>
+
+            <li><a id="reservation-cancel" class="reservation-cancel">annuler</a></li>
+        </ul>
+
+    </template>
+
+    <template id="templateError">
+        <li data-error-message="" class="errors__itm">Ici vient le message d'erreur</li>
+    </template>
+
+    <template id="templateMessage">
+        <li data-message="" class="messages__itm">Ici vient le message</li>
+    </template>
+    <!-- gym template -->
+    <template id="hallTemplate">
+        <option class="hall__option js-hall-option" value="" required>Veuillez choisir une option</option>
+    </template>
+
+    <!-- days template -->
+    <template id="day-template">
+        <li><a><time datetime="" data-date="" class="calender__month--day js-calender__month--day"></time></a></li>
+    </template>
+
+    <!-- hours template -->
+    <template id="hours-template">
+        <li><a><time class="hours__container--element js-hours__element" datetime="" data-hour="" data-minutes=""></time></a></li>
+    </template>
+
+
+    <template id="templateError">
+        <li data-error-message="" class="errors__itm">Ici vient le message d'erreur</li>
+    </template>
+
+    <template id="templateMessage">
+        <li data-message="" class="messages__itm">Ici vient le message</li>
+    </template>
+</main>
+
+<script type="module" src="./js/reservation/reservation.js"></script>
 
 <?php
 include 'includes/_footer.php';
 ?>
-
-<!-- template reservation -->
-
-<template id="template-reservation">
-    <table id="reservation-details-table" class="reservation-details-table">
-        <tr class="reservation-details-raw">
-            <th>Salle</th>
-            <td id="gym">Salle1</td>
-        </tr>
-        <tr class="reservation-details-raw">
-            <th>Date de réservation</th>
-            <td id="dateReservation">14/07/2024 8:30</td>
-        </tr>
-        <tr class="reservation-details-raw">
-            <th>Durée</th>
-            <td id="duration">1h</td>
-        </tr>
-        <tr class="reservation-details-raw">
-            <td id="totalPrix">50€</td>
-        </tr>
-        <tr class="reservation-details-raw">
-            <th>Statut</th>
-            <td id="status">modifier</td>
-        </tr>
-    </table>
-    <ul class="reservation-details-raw">
-        <li id="reservation-edit"><a class="reservation-edit">modifier</a></li>
-
-        <li><a id="reservation-cancel" class="reservation-cancel">annuler</a></li>
-    </ul>
-
-</template>
-
-<template id="templateError">
-    <li data-error-message="" class="errors__itm">Ici vient le message d'erreur</li>
-</template>
-
-<template id="templateMessage">
-    <li data-message="" class="messages__itm">Ici vient le message</li>
-</template>
 </body>
 
 </html>
